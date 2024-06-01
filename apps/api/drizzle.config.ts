b26e522a996f54466f68b0ab5cd9f4355c05c24b
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-06-01 00:28:36 -0500

    fix

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -4,9 +4,9 @@ export default defineConfig({
     schema: "./src/db/schema.ts",
     out: "./drizzle",
     dbCredentials: {
-        url: config.db.url,
+        url: config.db.url
     },
     verbose: true,
     strict: true,
-    dialect: "postgresql",
+    dialect: "postgresql"
 });
--- apps/api/src/db/schema.ts
+++ apps/api/src/db/schema.ts
@@ -2,25 +2,25 @@ import { relations } from "drizzle-orm";
 import { text, pgTable, varchar, bigint } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-    id: varchar("id", {length: 36}).primaryKey(),
-    email: varchar("email", {length: 255}).notNull().unique(),
+    id: varchar("id", { length: 36 }).primaryKey(),
+    email: varchar("email", { length: 255 }).notNull(),
     image_url: text("image_url"),
-    username: varchar("username", {length: 255}).unique(),
+    username: varchar("username", { length: 255 }).unique(),
     created_at: bigint("created_at", {
         mode: "number"
     }).notNull(),
     updated_at: bigint("updated_at", {
         mode: "number"
-    }).notNull(),
+    }).notNull()
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
-  posts: many(documents),
+    posts: many(documents)
 }));
 
 export const documents = pgTable("documents", {
-    id: varchar("id", {length: 255}).primaryKey(),
-    owner_id: varchar("owner_id", {length: 255}).notNull(),
+    id: varchar("id", { length: 255 }).primaryKey(),
+    owner_id: varchar("owner_id", { length: 255 }).notNull(),
     title: text("title"),
     content: text("content"),
     created_at: bigint("created_at", {
@@ -28,12 +28,12 @@ export const documents = pgTable("documents", {
     }).notNull(),
     updated_at: bigint("updated_at", {
         mode: "number"
-    }).notNull(),
-})
+    }).notNull()
+});
 
 export const documentRelations = relations(documents, ({ one }) => ({
-  author: one(users, {
-    fields: [documents.owner_id],
-    references: [users.id],
-  }),
+    author: one(users, {
+        fields: [documents.owner_id],
+        references: [users.id]
+    })
 }));
